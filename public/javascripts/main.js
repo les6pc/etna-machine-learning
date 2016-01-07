e9a8c0e0c6082d8347c53d805112e324bf6537f7
Romain Lefort <romain.lefort123@gmail.com>
2016-01-07 16:14:47 +0100

    finish front, missing data chart & hour

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -229,4 +229,73 @@ jQuery(document).ready(function($){
 		$("#averageFav").html(avFav);
 		$("#noFav").html(noFav);
 	}
+
+
+
+	window.onload = function () {
+		var chart = new CanvasJS.Chart("chartContainer",
+		{
+			axisY:{
+	        	title: "Nombre de tweets"
+	      	},
+
+	      	axisX:{
+	        	title: "Heure"
+	      	},
+			animationEnabled: true,
+			// title:{
+			// 	text: "Multi Series Spline Chart - Hide / Unhide via Legend"
+			// },
+			data: [
+			{
+				type: "spline", //change type to bar, line, area, pie, etc
+				showInLegend: true,
+				legendText: "Retweet",
+				color: "#e3ca76",   
+				dataPoints: [
+					{ x: 10, y: 51 },
+					{ x: 20, y: 45},
+					{ x: 30, y: 50 },
+					{ x: 40, y: 62 },
+					{ x: 50, y: 95 },
+					{ x: 60, y: 66 },
+					{ x: 70, y: 24 },
+					{ x: 80, y: 32 },
+					{ x: 90, y: 16}
+				]
+				},
+				{
+				type: "spline",
+				showInLegend: true,
+				legendText: "Favoris",
+				color: "#e74c3c",  
+				dataPoints: [
+					{ x: 10, y: 21 },
+					{ x: 20, y: 44},
+					{ x: 30, y: 35 },
+					{ x: 40, y: 45 },
+					{ x: 50, y: 75 },
+					{ x: 60, y: 58 },
+					{ x: 70, y: 18 },
+					{ x: 80, y: 30 },
+					{ x: 90, y: 11}
+				]
+				}
+			],
+			legend: {
+				cursor: "pointer",
+				itemclick: function (e) {
+					if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+						e.dataSeries.visible = false;
+					} else {
+						e.dataSeries.visible = true;
+				}
+				chart.render();
+				}
+			}
+		});
+
+		chart.render();
+		$('.canvasjs-chart-credit').addClass('hidden');
+	}
 });
