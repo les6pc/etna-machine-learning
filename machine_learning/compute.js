9fcc2f1adb1eaa287b72d78415cc4989896032df
EarvinKayonga <demoneartin@gmail.com>
2016-01-07 18:19:46 +0100

    Bundle Favicon symfony 2

--- machine_learning/compute.js
+++ machine_learning/compute.js
@@ -1,27 +1,32 @@
 var ml = require('machine_learning');
 var limdu = require('limdu');
-var colorClassifier = new limdu.classifiers.NeuralNetwork();
+var Classifier = new limdu.classifiers.NeuralNetwork();
 
 module.exports = {
   compute: function(tweets) {
+    var average = 0.4,
+      train = tweets.map(function(obj) {
+        var rObj = {
+          input: {
+            engagement: obj.engagement,
+            time: new Date(obj.created_at).getTime()
+          },
+          output: (obj.engagement >= average) ? 1 : 0
+        }
+        return rObj;
+      });
+    Classifier.trainBatch(train);
+    var Res = [],
+      index = 0;
+
+    Res[index] = Classifier.classify({
+      engagement: 1,
+      time: new Date().getTime()
+    })
+
     return {
-      matin : "09:15",
-      soir: "18:40"
+      matin: "08:34",
+      soir: "18:30"
     };
   }
 }
-
-colorClassifier.trainBatch([
-    {input: { r: 0.03, g: 0.7, b: 0.5 }, output: 0},  // black
-    {input: { r: 0.16, g: 0.09, b: 0.2 }, output: 1}, // white
-    {input: { r: 0.5, g: 0.5, b: 1.0 }, output: 1}   // white
-    ]);
-
-console.log(colorClassifier.classify({ r: 1, g: 0.4, b: 0 }));  // 0.99 - almost white
-
-
-var birdClassifier = new limdu.classifiers.Winnow({
-    default_positive_weight: 1,
-    default_negative_weight: 1,
-    threshold: 0
-});
